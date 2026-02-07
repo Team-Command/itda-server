c80e3906b0f02f8325c4f705dd1214e742ac32b5
oijwef098234 <oijwef098234@gmail.com>
2026-02-07 23:48:56 +0900

    feat: initial commit

---
+++ src/main/java/com/command/itdaserver/ItdaServerApplication.java
@@ -0,0 +1,13 @@
+package com.command.itdaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItdaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItdaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/command/itdaserver/ItdaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.command.itdaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItdaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
