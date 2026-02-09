4716cb4b13291782b14c629bff8ad3e55cde501b
oijwef098234 <oijwef098234@gmail.com>
2026-02-09 17:09:05 +0900

    feat: ErrorMessage관련 생성자 문제 해결

--- src/main/java/com/command/itdaserver/global/error/exception/ItdaException.java
+++ src/main/java/com/command/itdaserver/global/error/exception/ItdaException.java
@@ -1,14 +1,14 @@
 package com.command.itdaserver.global.error.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 public class ItdaException extends RuntimeException {
     private final ErrorCode errorCode;
-
+    public ItdaException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
+        this.errorCode = errorCode;
+    }
     public ItdaException(ErrorCode errorCode, String customMessage) {
         super(customMessage);
         this.errorCode = errorCode;
