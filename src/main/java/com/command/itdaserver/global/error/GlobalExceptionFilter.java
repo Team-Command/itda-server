f5e6339b919f6f5c143871d655b4f687e42a0d86
oijwef098234 <oijwef098234@gmail.com>
2026-02-09 15:13:34 +0900

    feat: 글로벌 예외 클래스 생성

---
+++ src/main/java/com/command/itdaserver/global/error/GlobalExceptionFilter.java
@@ -0,0 +1,46 @@
+package com.command.itdaserver.global.error;
+
+import com.command.itdaserver.global.error.exception.ErrorCode;
+import com.command.itdaserver.global.error.exception.ItdaException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class GlobalExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    private static final String CONTENT_TYPE = "application/json";
+    private static final String CHARACTER_ENCODING = "UTF-8";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    )throws IOException {
+
+        try {
+            filterChain.doFilter(request,response);
+        } catch (ItdaException e){
+            ErrorCode errorCode = e.getErrorCode();
+            writerErrorResponse(response, errorCode.getStatusCode(), ErrorResponse.of(errorCode, errorCode.getErrorMessage()));
+        } catch (Exception e){
+            e.printStackTrace();
+            writerErrorResponse(response, response.getStatus(), ErrorResponse.of(response.getStatus(),e.getMessage()));
+        }
+    }
+
+    private void writerErrorResponse(HttpServletResponse response, int statusCode, ErrorResponse errorResponse) throws IOException{
+        response.setStatus(statusCode);
+        response.setContentType(CONTENT_TYPE);
+        response.setCharacterEncoding(CHARACTER_ENCODING);
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+    }
+}
