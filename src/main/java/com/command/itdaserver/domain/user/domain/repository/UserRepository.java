fc1cb9ccccdb1aae7ab96e00c3fe7bbc28f35e74
redjungi <redjungi09@gmail.com>
2026-02-11 11:36:57 +0900

    feat: User 리포지토리 생성

---
+++ src/main/java/com/command/itdaserver/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.command.itdaserver.domain.user.domain.repository;
+
+import com.command.itdaserver.domain.user.domain.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUserId(@NotBlank(message = "유저아이디는 필수입니다.") String s);
+
+    boolean existsByEmail(@NotBlank(message = "이메일은 필수입니다.") @Email(message = "올바른 이메일 형식이 아닙니다.") String email);
+}
