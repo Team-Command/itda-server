74df7c55b252c0e326f2b618f88ae560b226e412
redjungi <redjungi09@gmail.com>
2026-02-11 21:01:48 +0900

    feat: 로그인 API 컨트롤러 작성

--- src/main/java/com/command/itdaserver/domain/auth/presentation/AuthController.java
+++ src/main/java/com/command/itdaserver/domain/auth/presentation/AuthController.java
@@ -1,9 +1,14 @@
 package com.command.itdaserver.domain.auth.presentation;
 
+import com.command.itdaserver.domain.auth.presentation.dto.request.LoginRequest;
 import com.command.itdaserver.domain.auth.presentation.dto.request.SignUpRequest;
+import com.command.itdaserver.domain.auth.presentation.dto.response.LoginResponse;
 import com.command.itdaserver.domain.auth.presentation.dto.response.SignUpResponse;
+import com.command.itdaserver.domain.auth.service.LoginService;
 import com.command.itdaserver.domain.auth.service.SignUpService;
 import com.command.itdaserver.domain.auth.service.command.SignUpCommand;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final SignUpService signUpService;
+    private final LoginService loginService;
 
     @PostMapping("/signup")
     public ResponseEntity<SignUpResponse> signUp(@Valid @RequestBody SignUpRequest request){
@@ -28,4 +34,28 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new SignUpResponse("회원가입이 되었습니다, 로그인 후 서비스를 이용해주세요."));
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(
+            @RequestBody LoginRequest request,
+            HttpServletResponse response
+    ){
+        String sessionId = loginService.execute(request);
+
+        addSessionCookie(response, sessionId);
+
+        return ResponseEntity.ok(new LoginResponse("로그인 성공"));
+    }
+
+    private void addSessionCookie(HttpServletResponse response, String sessionId) {
+        Cookie cookie = new Cookie("SESSION_ID", sessionId);
+        cookie.setHttpOnly(true);      // XSS 방어
+        cookie.setSecure(true);        // HTTPS만 전송
+        cookie.setPath("/");           // 모든 경로에서 전송
+        cookie.setMaxAge(1800);        // 30분
+
+
+        response.addCookie(cookie);
+    }
+
 }
