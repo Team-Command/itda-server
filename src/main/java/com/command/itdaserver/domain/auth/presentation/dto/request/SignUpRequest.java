0b6894e82799180a8367427b339f2b584eac3cbd
redjungi <redjungi09@gmail.com>
2026-02-11 13:53:18 +0900

    feat: 유저아이디 정책, 비밀번호 정책 검증 로직 생성

--- src/main/java/com/command/itdaserver/domain/auth/presentation/dto/request/SignUpRequest.java
+++ src/main/java/com/command/itdaserver/domain/auth/presentation/dto/request/SignUpRequest.java
@@ -4,9 +4,7 @@ import com.command.itdaserver.domain.user.domain.enums.Grade;
 import com.command.itdaserver.domain.user.domain.enums.Major;
 import com.command.itdaserver.domain.user.domain.enums.School;
 import com.command.itdaserver.global.validation.ValidCustomMajor;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 @ValidCustomMajor
 public record SignUpRequest(
@@ -14,11 +12,19 @@ public record SignUpRequest(
         String name,
 
         @NotBlank(message = "유저아이디는 필수입니다.")
-        //유저아이디 규칙 상의 예정
+        @Size(min = 5, max = 15, message = "아이디는 5자 이상 15자 이하여야 합니다.")
+        @Pattern(
+                regexp = "^(?=.*[a-zA-Z])[a-zA-Z0-9_.]+$",
+                message = "아이디는 영문, 숫자, 언더바(_), 마침표(.)만 사용 가능하며, 영문은 최소 1자 이상 포함되어야 합니다."
+        )
         String userId,
 
         @NotBlank(message = "비밀번호는 필수입니다.")
-        //비밀번호 규칙 상의 예정
+        @Size(min = 8, max = 30, message = "비밀번호는 8자 이상 20자 이하여야 합니다.")
+        @Pattern(
+                regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]+$",
+                message = "비밀번호는 영문, 숫자, 특수문자(@$!%*?&#)를 각각 최소 1자 이상 포함해야 합니다."
+        )
         String password,
 
         @NotBlank(message = "이메일은 필수입니다.")
