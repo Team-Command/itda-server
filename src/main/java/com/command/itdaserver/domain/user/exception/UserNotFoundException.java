f280373ace469e3a7516df60db37395bf992130c
oijwef098234 <oijwef098234@gmail.com>
2026-02-12 20:12:44 +0900

    feat: 잘못된 예외처리 해결

--- src/main/java/com/command/itdaserver/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/command/itdaserver/domain/user/exception/UserNotFoundException.java
@@ -5,7 +5,7 @@ import com.command.itdaserver.global.error.exception.ErrorCode;
 import com.command.itdaserver.global.error.exception.ItdaException;
 
 public class UserNotFoundException extends ItdaException {
-    public static final ItdaException EXCEPTION = new DuplicateEmailException();
+    public static final ItdaException EXCEPTION = new UserNotFoundException();
 
     public UserNotFoundException() {
         super(ErrorCode.USER_NOT_FOUND);
