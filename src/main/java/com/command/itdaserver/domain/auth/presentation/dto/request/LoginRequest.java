cedf2fec3930ed605eaf09256820c72693df72b5
redjungi <redjungi09@gmail.com>
2026-02-13 17:08:55 +0900

    feat: LoginRequest에 rememberMe추가

--- src/main/java/com/command/itdaserver/domain/auth/presentation/dto/request/LoginRequest.java
+++ src/main/java/com/command/itdaserver/domain/auth/presentation/dto/request/LoginRequest.java
@@ -3,6 +3,7 @@ package com.command.itdaserver.domain.auth.presentation.dto.request;
 
 public record LoginRequest (
         String userId,
-        String password
+        String password,
+        boolean rememberMe
 ){
 }
