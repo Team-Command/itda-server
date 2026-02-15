c057613c9a27ac08d3aa3bfcb192a1460c9fd5f7
oijwef098234 <oijwef098234@gmail.com>
2026-02-15 17:22:44 +0900

    chore: 불필요한 import문 제거

--- src/main/java/com/command/itdaserver/domain/auth/service/LogoutService.java
+++ src/main/java/com/command/itdaserver/domain/auth/service/LogoutService.java
@@ -2,7 +2,6 @@ package com.command.itdaserver.domain.auth.service;
 
 import com.command.itdaserver.domain.auth.domain.repository.RememberMeRepository;
 import com.command.itdaserver.domain.auth.domain.repository.SessionRepository;
-import com.command.itdaserver.domain.auth.presentation.dto.response.LogoutResponse;
 import com.command.itdaserver.global.auth.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
