99f97249889d340097c02ec5c3c5bfcb19ed7042
oijwef098234 <oijwef098234@gmail.com>
2026-02-16 23:38:53 +0900

    feat: 클라 단 쿠키 자동 삭제 구현

--- src/main/java/com/command/itdaserver/domain/user/presentation/UserController.java
+++ src/main/java/com/command/itdaserver/domain/user/presentation/UserController.java
@@ -3,6 +3,9 @@ package com.command.itdaserver.domain.user.presentation;
 import com.command.itdaserver.domain.user.presentation.dto.response.DeleteUserResponse;
 import com.command.itdaserver.domain.user.service.DeleteUserAccountService;
 import com.command.itdaserver.global.auth.CustomUserDetails;
+import com.command.itdaserver.global.util.CookieUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,11 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
+    private final CookieUtil cookieUtil;
+
     private final DeleteUserAccountService deleteUserAccount;
 
     @DeleteMapping
-    public ResponseEntity<DeleteUserResponse> deleteUserAccount(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    public ResponseEntity<DeleteUserResponse> deleteUserAccount(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            HttpServletResponse response
+    ) {
         deleteUserAccount.execute(customUserDetails);
+
+        cookieUtil.removeSessionCookie(response);
+        cookieUtil.removeRememberMeCookie(response);
 
         return ResponseEntity.ok(new DeleteUserResponse("회원 탈퇴가 완료되었습니다"));
     }
