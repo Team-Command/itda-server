949c8b0ef9265b264f43ef2e372ec92441105ffd
oijwef098234 <oijwef098234@gmail.com>
2026-02-16 21:38:28 +0900

    feat: 로그아웃, 회원 탈퇴시 시큐리티 컨텍스트 클리어 코드 추가

--- src/main/java/com/command/itdaserver/domain/user/presentation/UserController.java
+++ src/main/java/com/command/itdaserver/domain/user/presentation/UserController.java
@@ -1,22 +1,20 @@
 package com.command.itdaserver.domain.user.presentation;
 
-import com.command.itdaserver.domain.auth.presentation.dto.response.LogoutResponse;
 import com.command.itdaserver.domain.user.presentation.dto.response.DeleteUserResponse;
-import com.command.itdaserver.domain.user.service.DeleteUserAccount;
+import com.command.itdaserver.domain.user.service.DeleteUserAccountService;
 import com.command.itdaserver.global.auth.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-    private final DeleteUserAccount deleteUserAccount;
+    private final DeleteUserAccountService deleteUserAccount;
 
     @DeleteMapping
     public ResponseEntity<DeleteUserResponse> deleteUserAccount(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
--- src/main/java/com/command/itdaserver/domain/user/service/DeleteUserAccountService.java
+++ src/main/java/com/command/itdaserver/domain/user/service/DeleteUserAccountService.java
@@ -4,12 +4,13 @@ import com.command.itdaserver.domain.auth.domain.repository.SessionRepository;
 import com.command.itdaserver.domain.user.domain.repository.UserRepository;
 import com.command.itdaserver.global.auth.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class DeleteUserAccount {
+public class DeleteUserAccountService {
     private final UserRepository userRepository;
     private final SessionRepository sessionRepository;
 
@@ -17,7 +18,9 @@ public class DeleteUserAccount {
     public void execute(CustomUserDetails customUserDetails) {
         String deleteUserId = customUserDetails.getUserId();
 
-        userRepository.deleteByUserId(deleteUserId);
         sessionRepository.deleteByUserId(deleteUserId);
+        userRepository.deleteByUserId(deleteUserId);
+
+        SecurityContextHolder.clearContext();
     }
 }
