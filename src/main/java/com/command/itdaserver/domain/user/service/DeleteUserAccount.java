ec1fe598f3c11b8bc9d58bc163545653a1eff28e
oijwef098234 <oijwef098234@gmail.com>
2026-02-16 18:30:53 +0900

    feat: 회원 삭제 메서드 구현

--- src/main/java/com/command/itdaserver/domain/user/service/DeleteUserAccount.java
+++ src/main/java/com/command/itdaserver/domain/user/service/DeleteUserAccount.java
@@ -1,6 +1,7 @@
 package com.command.itdaserver.domain.user.service;
 
 import com.command.itdaserver.domain.user.domain.repository.UserRepository;
+import com.command.itdaserver.global.auth.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,9 @@ import org.springframework.stereotype.Service;
 public class DeleteUserAccount {
     private final UserRepository userRepository;
 
-    public void execute() {
+    public void execute(CustomUserDetails customUserDetails) {
+        String deleteUserId = customUserDetails.getUserId();
 
+        userRepository.deleteByUserId(deleteUserId);
     }
 }
