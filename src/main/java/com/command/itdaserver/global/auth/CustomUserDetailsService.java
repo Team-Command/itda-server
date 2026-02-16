36cc8f3bc74441a38483309db0b702226fa86d5c
oijwef098234 <oijwef098234@gmail.com>
2026-02-16 23:59:35 +0900

    chore: 불필요한 import문 제거

--- src/main/java/com/command/itdaserver/global/auth/CustomUserDetailsService.java
+++ src/main/java/com/command/itdaserver/global/auth/CustomUserDetailsService.java
@@ -2,14 +2,11 @@ package com.command.itdaserver.global.auth;
 
 import com.command.itdaserver.domain.user.domain.User;
 import com.command.itdaserver.domain.user.domain.repository.UserRepository;
-import com.command.itdaserver.domain.user.exception.UserNotMatchedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import javax.naming.AuthenticationException;
 
 @Service
 @RequiredArgsConstructor
