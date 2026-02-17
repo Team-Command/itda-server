0ed92e75daf6fd6598bc17f1643a27b7b11e3250
redjungi <redjungi09@gmail.com>
2026-02-18 00:18:22 +0900

    feat: 코드 email발송 서비스 작성

---
+++ src/main/java/com/command/itdaserver/domain/auth/service/PasswordResetService.java
@@ -0,0 +1,38 @@
+package com.command.itdaserver.domain.auth.service;
+
+import com.command.itdaserver.domain.auth.domain.EmailVerification;
+import com.command.itdaserver.domain.auth.domain.repository.EmailVerificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordResetService {
+    private final EmailVerificationRepository emailVerificationRepository;
+    private final JavaMailSender mailSender;
+
+    public void execute(String email){
+        String code = generateCode();
+
+        EmailVerification verification = EmailVerification.create(email, code);
+        emailVerificationRepository.save(verification);
+
+        sendEmail(email, code);
+    }
+
+    private String generateCode() {
+        return String.format("%06d", new Random().nextInt(1000000));
+    }
+
+    private void sendEmail(String email, String code) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("[itda] 비밀번호 재설정 인증 코드 : " + code);
+        message.setText("인증 코드: " + code + "\n\n5분 이내에 입력해주세요.");
+        mailSender.send(message);
+    }
+}
