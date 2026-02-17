9c0dd499a542792a483647fc403721ed8d5cc38e
oijwef098234 <oijwef098234@gmail.com>
2026-02-17 11:40:46 +0900

    feat: 띄어쓰기 수정

--- src/main/java/com/command/itdaserver/domain/user/presentation/dto/response/UserResponse.java
+++ src/main/java/com/command/itdaserver/domain/user/presentation/dto/response/UserResponse.java
@@ -10,23 +10,23 @@ import lombok.Getter;
 @Builder
 @Getter
 public class UserResponse{
-        private String name;
-        private String userId;
-        private String email;
-        private Major major;
-        private String customMajor;
-        private School school;
-        private Grade grade;
+    private String name;
+    private String userId;
+    private String email;
+    private Major major;
+    private String customMajor;
+    private School school;
+    private Grade grade;
 
-        public static UserResponse from(User user){
-            return UserResponse.builder()
-                    .name(user.getName())
-                    .userId(user.getUserId())
-                    .email(user.getEmail())
-                    .major(user.getMajor())
-                    .customMajor(user.getCustomMajor())
-                    .school(user.getSchool())
-                    .grade(user.getGrade())
-                    .build();
-        }
+     public static UserResponse from(User user){
+          return UserResponse.builder()
+               .name(user.getName())
+               .userId(user.getUserId())
+               .email(user.getEmail())
+               .major(user.getMajor())
+               .customMajor(user.getCustomMajor())
+               .school(user.getSchool())
+               .grade(user.getGrade())
+               .build();
+     }
 }
