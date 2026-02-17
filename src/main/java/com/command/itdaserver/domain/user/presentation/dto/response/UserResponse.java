8ba59f610ff40c72b92a6c8e36327bcdd85ffb92
oijwef098234 <oijwef098234@gmail.com>
2026-02-18 01:36:09 +0900

    feat: 클래스 레코드로 변경

--- src/main/java/com/command/itdaserver/domain/user/presentation/dto/response/UserResponse.java
+++ src/main/java/com/command/itdaserver/domain/user/presentation/dto/response/UserResponse.java
@@ -4,29 +4,26 @@ import com.command.itdaserver.domain.user.domain.User;
 import com.command.itdaserver.domain.user.domain.enums.Grade;
 import com.command.itdaserver.domain.user.domain.enums.Major;
 import com.command.itdaserver.domain.user.domain.enums.School;
-import lombok.Builder;
-import lombok.Getter;
 
-@Builder
-@Getter
-public class UserResponse{
-    private String name;
-    private String userId;
-    private String email;
-    private Major major;
-    private String customMajor;
-    private School school;
-    private Grade grade;
+public record UserResponse(
+        String name,
+        String userId,
+        String email,
+        Major major,
+        String customMajor,
+        School school,
+        Grade grade
+) {
 
-     public static UserResponse from(User user){
-          return UserResponse.builder()
-               .name(user.getName())
-               .userId(user.getUserId())
-               .email(user.getEmail())
-               .major(user.getMajor())
-               .customMajor(user.getCustomMajor())
-               .school(user.getSchool())
-               .grade(user.getGrade())
-               .build();
-     }
+    public static UserResponse from(User user) {
+        return new UserResponse(
+                user.getName(),
+                user.getUserId(),
+                user.getEmail(),
+                user.getMajor(),
+                user.getCustomMajor(),
+                user.getSchool(),
+                user.getGrade()
+        );
+    }
 }
--- src/main/java/com/command/itdaserver/domain/user/service/QueryMyProfileService.java
+++ src/main/java/com/command/itdaserver/domain/user/service/QueryMyProfileService.java
@@ -4,7 +4,6 @@ import com.command.itdaserver.domain.user.domain.User;
 import com.command.itdaserver.domain.user.domain.repository.UserRepository;
 import com.command.itdaserver.domain.user.exception.UserNotFoundException;
 import com.command.itdaserver.domain.user.presentation.dto.response.UserResponse;
-import com.command.itdaserver.global.auth.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
