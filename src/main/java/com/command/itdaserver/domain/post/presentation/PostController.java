95916ea4098a20ddaa2e6242aca5ed3fc3763b55
itzjb <gogror0987@gmail.com>
2026-02-18 17:07:26 +0900

    fix: Post 관련 반환값을  PostResponse DTO 로 재구성

--- src/main/java/com/command/itdaserver/domain/post/presentation/PostController.java
+++ src/main/java/com/command/itdaserver/domain/post/presentation/PostController.java
@@ -3,30 +3,42 @@ package com.command.itdaserver.domain.post.presentation;
 import com.command.itdaserver.domain.post.domain.Post;
 import com.command.itdaserver.domain.post.domain.Question;
 import com.command.itdaserver.domain.post.presentation.dto.request.CreateFormRequest;
-import com.command.itdaserver.domain.post.presentation.dto.request.CreateFormRequest.QuestionDto;
 import com.command.itdaserver.domain.post.presentation.dto.request.CreatePostRequest;
+import com.command.itdaserver.domain.post.presentation.dto.response.PostResponse;
 import com.command.itdaserver.domain.post.service.CreateApplyFormService;
 import com.command.itdaserver.domain.post.service.CreatePostService;
 import com.command.itdaserver.domain.post.service.GetPostService;
 import com.command.itdaserver.global.auth.CustomUserDetails;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/projects")
+@RestController
 public class PostController {
 
     private final GetPostService getPostService;
+    private final CreatePostService createPostService;
+    private final CreateApplyFormService createApplyFormService;
 
     @GetMapping("/{postId}")
-    public Post getPost(@PathVariable Long postId) {
-        return getPostService.getPost(postId);
+    public PostResponse getPost(@PathVariable Long postId) {
+        return new PostResponse(getPostService.getPost(postId));
+    }
+
+    @PostMapping("/post")
+    public PostResponse createPost(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                   @Valid @RequestBody CreatePostRequest request) {
+        return new PostResponse(createPostService.execute(request, customUserDetails));
+    }
+
+    @PostMapping("/{postId}/apply-form")
+    public List<Question> createApplyForm(@PathVariable Long postId, @Valid @RequestBody CreateFormRequest request) {
+        return createApplyFormService.execute(postId, request);
     }
 
 }
---
+++ src/main/java/com/command/itdaserver/domain/post/presentation/dto/response/PostResponse.java
@@ -0,0 +1,31 @@
+package com.command.itdaserver.domain.post.presentation.dto.response;
+
+import com.command.itdaserver.domain.post.domain.Post;
+import com.command.itdaserver.domain.post.domain.Question;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class PostResponse {
+
+    private Long postId;
+    private String writer;
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime applyDeadline;
+    private List<Question> questions = new ArrayList<>();
+
+    public PostResponse(Post post) {
+        this.postId = post.getId();
+        this.writer = post.getWriter().getUserId();
+        this.title = post.getTitle();
+        this.description = post.getDescription();
+        this.createdAt = post.getCreatedAt();
+        this.applyDeadline = post.getApplyDeadline();
+        this.questions = post.getQuestions();
+    }
+}
