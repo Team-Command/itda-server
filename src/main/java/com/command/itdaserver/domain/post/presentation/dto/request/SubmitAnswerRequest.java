0daeed58ba167db29893c804d18f820032f0659e
itzjb <gogror0987@gmail.com>
2026-02-18 23:21:55 +0900

    fix: SubmitAnswerRequest DTO 중 answer 부분 중첩 검증 추가

--- src/main/java/com/command/itdaserver/domain/post/presentation/dto/request/SubmitAnswerRequest.java
+++ src/main/java/com/command/itdaserver/domain/post/presentation/dto/request/SubmitAnswerRequest.java
@@ -1,5 +1,7 @@
 package com.command.itdaserver.domain.post.presentation.dto.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -9,6 +11,8 @@ import java.util.List;
 @Data
 public class SubmitAnswerRequest {
 
+    @Valid
+    @NotEmpty(message = "답변 목록은 비어있을 수 없습니다.")
     private List<AnswerDto> answers = new ArrayList<>();
 
     @Data
