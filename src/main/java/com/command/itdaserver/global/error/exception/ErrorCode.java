43f35ded7029c6ce2abcdb367e8abfa32831e6c6
redjungi <redjungi09@gmail.com>
2026-02-18 14:21:25 +0900

    feat: resetToken에 관련한 에러코드 작성

--- src/main/java/com/command/itdaserver/global/error/exception/ErrorCode.java
+++ src/main/java/com/command/itdaserver/global/error/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     //user
     INVALID_USER(401, "유효 하지 않은 사용자입니다."),
     INVALID_CUSTOM_MAJOR(400, "기타 전공 선택 시 전공명을 입력해야 합니다."),
+    USER_NOT_FOUND(404,"사용자를 찾을 수 없습니다."),
 
     // auth - 새로 추가!
     DUPLICATE_USER_ID(409, "이미 존재하는 아이디입니다."),
@@ -18,7 +19,7 @@ public enum ErrorCode {
     USER_NOT_MATCHED(401, "아이디 또는 비밀번호를 확인해주세요."),
     VERIFICATION_CODE_EXPIRED(404, "인증 코드가 만료되었습니다."),
     VERIFICATION_CODE_NOT_MATCHED(400, "인증 코드가 일치하지 않습니다."),
-
+    RESET_TOKEN_NOT_FOUND(401, "유효하지 않은 비밀번호 재설정 토큰입니다."),
 
     // general
     BAD_REQUEST(400, "잘못된 요청입니다."),
