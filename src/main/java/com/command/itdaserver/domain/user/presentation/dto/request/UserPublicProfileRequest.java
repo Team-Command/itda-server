b28203e84614f8eec67b4fe4beb0672b5c280223
oijwef098234 <oijwef098234@gmail.com>
2026-02-23 12:09:15 +0900

    feat : 네이밍 변경

--- src/main/java/com/command/itdaserver/domain/user/presentation/dto/request/UserPublicProfileRequest.java
+++ src/main/java/com/command/itdaserver/domain/user/presentation/dto/request/UserPublicProfileRequest.java
@@ -1,11 +1,11 @@
 package com.command.itdaserver.domain.user.presentation.dto.request;
 
 public record UserPublicProfileRequest(
-        boolean name,
-        boolean email,
-        boolean major,
-        boolean customMajor,
-        boolean grade,
-        boolean school
+        boolean isNamePublic,
+        boolean isEmailPublic,
+        boolean isMajorPublic,
+        boolean isCustomMajorPublic,
+        boolean isGradePublic,
+        boolean isSchoolPublic
 ) {
 }
--- src/main/java/com/command/itdaserver/domain/user/service/UserProfileDisclosureService.java
+++ src/main/java/com/command/itdaserver/domain/user/service/UserProfileDisclosureService.java
@@ -1,6 +1,7 @@
 package com.command.itdaserver.domain.user.service;
 
 import com.command.itdaserver.domain.user.domain.repository.UserRepository;
+import com.command.itdaserver.domain.user.presentation.dto.request.UserPublicProfileRequest;
 import com.command.itdaserver.domain.user.presentation.dto.response.UserPublicProfileResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class UserProfileDisclosureService {
     private final UserRepository userRepository;
 
-    public UserPublicProfileResponse execute(){
+    public UserPublicProfileResponse execute(UserPublicProfileRequest request) {
 
     }
 }
