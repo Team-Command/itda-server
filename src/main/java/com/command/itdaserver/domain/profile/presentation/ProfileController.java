7c8224be6ca0f9c9087e1a4f3ad0dc8b5d289c28
oijwef098234 <oijwef098234@gmail.com>
2026-02-23 13:44:44 +0900

    feat : 메시지 반환 dto 생성

--- src/main/java/com/command/itdaserver/domain/profile/presentation/ProfileController.java
+++ src/main/java/com/command/itdaserver/domain/profile/presentation/ProfileController.java
@@ -3,10 +3,7 @@ package com.command.itdaserver.domain.profile.presentation;
 import com.command.itdaserver.domain.profile.service.QueryUserProfileService;
 import com.command.itdaserver.domain.user.presentation.dto.response.UserResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/profile")
@@ -18,4 +15,7 @@ public class ProfileController {
     public UserResponse queryUserProfile(@PathVariable String userId) {
         return queryUserProfileService.execute(userId);
     }
+
+    @PatchMapping("/visability")
+    public
 }
