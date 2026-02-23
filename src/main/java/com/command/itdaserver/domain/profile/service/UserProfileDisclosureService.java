8d771bedec81dd59eb35b4166ed54334f0834c2e
oijwef098234 <oijwef098234@gmail.com>
2026-02-23 12:23:01 +0900

    feat : repository 생성

--- src/main/java/com/command/itdaserver/domain/profile/service/UserProfileDisclosureService.java
+++ src/main/java/com/command/itdaserver/domain/profile/service/UserProfileDisclosureService.java
@@ -1,5 +1,6 @@
 package com.command.itdaserver.domain.profile.service;
 
+import com.command.itdaserver.domain.user.domain.repository.UserDisclosureRepository;
 import com.command.itdaserver.domain.user.domain.repository.UserRepository;
 import com.command.itdaserver.domain.profile.presentation.dto.request.UserPublicProfileRequest;
 import com.command.itdaserver.domain.profile.presentation.dto.response.UserPublicProfileResponse;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserProfileDisclosureService {
     private final UserRepository userRepository;
+    private final UserDisclosureRepository userDisclosureRepository;
 
     public UserPublicProfileResponse execute(UserPublicProfileRequest request) {
 
---
+++ src/main/java/com/command/itdaserver/domain/user/domain/repository/UserDisclosureRepository.java
@@ -0,0 +1,7 @@
+package com.command.itdaserver.domain.user.domain.repository;
+
+import com.command.itdaserver.domain.user.domain.UserDisclosure;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDisclosureRepository extends JpaRepository<UserDisclosure, Long> {
+}
