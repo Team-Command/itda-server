9a67eec9e7f802a6a9794433e349eb5ede919fe3
oijwef098234 <oijwef098234@gmail.com>
2026-02-23 09:10:46 +0900

    feat: 공개여부 판단 Entity 생성

--- src/main/java/com/command/itdaserver/domain/user/domain/UserDisclosure.java
+++ src/main/java/com/command/itdaserver/domain/user/domain/UserDisclosure.java
@@ -1,4 +1,36 @@
 package com.command.itdaserver.domain.user.domain;
 
-public class UserDisclosure {
+import com.command.itdaserver.global.entity.BaseIdEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users_disclosure")
+@AttributeOverride(name = "id", column = @Column(name = "ud_id"))
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class UserDisclosure extends BaseIdEntity {
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "u_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(nullable = false)
+    private boolean name;
+
+    @Column(nullable = false)
+    private boolean email;
+
+    @Column(nullable = false)
+    private boolean major;
+
+    @Column(nullable = false)
+    private boolean customMajor;
+
+    @Column(nullable = false)
+    private boolean school;
+
+    @Column(nullable = false)
+    private boolean grade;
 }
