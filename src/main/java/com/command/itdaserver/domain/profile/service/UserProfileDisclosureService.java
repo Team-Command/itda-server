e29cd81f14fd2414ee9d65d8a706454720ae6657
oijwef098234 <oijwef098234@gmail.com>
2026-02-23 13:53:03 +0900

    feat : 어노테이션 수정

--- src/main/java/com/command/itdaserver/domain/profile/service/UserProfileDisclosureService.java
+++ src/main/java/com/command/itdaserver/domain/profile/service/UserProfileDisclosureService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public class UserProfileDisclosureService {
     private final UserRepository userRepository;
     private final UserDisclosureRepository userDisclosureRepository;
