a00b7e003ef9c6a3ad9daad873c3ac0bb45320a7
oijwef098234 <oijwef098234@gmail.com>
2026-02-25 18:14:04 +0900

    fix: 불필요한 DTO 제거

--- src/main/java/com/command/itdaserver/domain/profile/presentation/dto/response/MessageResponse.java
+++
@@ -1,8 +0,0 @@
-package com.command.itdaserver.domain.profile.presentation.dto.response;
-
-public record MessageResponse(String message) {
-
-    public static MessageResponse of(String message) {
-        return new MessageResponse(message);
-    }
-}
