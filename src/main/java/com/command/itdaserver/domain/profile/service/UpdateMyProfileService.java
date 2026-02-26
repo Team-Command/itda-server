2842b15929849415525541bec1b24ed82ca8915b
oijwef098234 <oijwef098234@gmail.com>
2026-02-26 18:24:41 +0900

    feat: 수정 요청 DTO 생성

---
+++ src/main/java/com/command/itdaserver/domain/profile/service/UpdateMyProfileService.java
@@ -0,0 +1,19 @@
+package com.command.itdaserver.domain.profile.service;
+
+import com.command.itdaserver.domain.profile.presentation.dto.request.UserProfileRequest;
+import com.command.itdaserver.domain.user.domain.repository.UserDisclosureRepository;
+import com.command.itdaserver.domain.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateMyProfileService {
+    private final UserRepository userRepository;
+    private final UserDisclosureRepository userDisclosureRepository;
+
+    public void execute(UserProfileRequest request){
+        userRepository.findByUserId(request.userId())
+                .or(() -> )
+    }
+}
