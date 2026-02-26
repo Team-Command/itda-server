7b93c7c9989a172aef0c139915925c1cd085a406
oijwef098234 <oijwef098234@gmail.com>
2026-02-26 21:31:36 +0900

    feat: 회원탈퇴 로직 개선

--- src/main/java/com/command/itdaserver/domain/user/service/DeleteUserAccountService.java
+++ src/main/java/com/command/itdaserver/domain/user/service/DeleteUserAccountService.java
@@ -2,6 +2,8 @@ package com.command.itdaserver.domain.user.service;
 
 import com.command.itdaserver.domain.auth.domain.repository.SessionRepository;
 import com.command.itdaserver.domain.user.domain.User;
+import com.command.itdaserver.domain.user.domain.UserDisclosure;
+import com.command.itdaserver.domain.user.domain.repository.UserDisclosureRepository;
 import com.command.itdaserver.domain.user.domain.repository.UserRepository;
 import com.command.itdaserver.domain.user.exception.UserNotFoundException;
 import com.command.itdaserver.global.auth.CustomUserDetails;
@@ -14,12 +16,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DeleteUserAccountService {
     private final UserRepository userRepository;
+    private final UserDisclosureRepository userDisclosureRepository;
     private final SessionRepository sessionRepository;
 
     @Transactional
     public void execute(CustomUserDetails customUserDetails) {
 
         userRepository.findById(customUserDetails.getId()).ifPresent(deleteUser -> {
+            userDisclosureRepository.deleteById(deleteUser.getId());
             sessionRepository.deleteByUserId(deleteUser.getUserId());
             userRepository.delete(deleteUser);
         });
