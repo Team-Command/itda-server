ae44b54653ac6286043826a46288cfacb473da67
oijwef098234 <oijwef098234@gmail.com>
2026-03-18 20:00:57 +0900

    feat: 유저프로필 조회반환 dto에 PKid 추가

---
+++ src/main/java/com/command/itdaserver/domain/chat/service/CreateChatRoomService.java
@@ -0,0 +1,13 @@
+package com.command.itdaserver.domain.chat.service;
+
+import com.command.itdaserver.domain.chat.domain.repository.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateChatRoomService {
+    private final ChatRoomRepository chatRoomRepository;
+
+    
+}
